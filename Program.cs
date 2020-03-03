3bf2081bb8edbd6fbb4db7f1b99e7ab74be12dde
Daniel Sutcliffe <dannysutcliffe@hotmail.com>
2020-03-03 15:49:05 +0000

    Square sum

--- Program.cs
+++ Program.cs
@@ -2,11 +2,42 @@
 
 namespace cn_week9_c_sharp_inheritance
 {
+    class ObjectWeWillMessAroundWithTodayInTheClass
+    {
+        public void theWidth(int myWidth)
+        {
+            width = myWidth;
+        }
+        public void theHeight(int myHeight)
+        {
+            height = myHeight;
+        }
+
+        protected int width;
+        protected int height;
+    }
+
+    class Square : ObjectWeWillMessAroundWithTodayInTheClass
+    // subclass created here.
+    {
+        public int area() {
+            // void not required as now returning something
+            return (width * height);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // creating a new instance of square
+            Square mySquare = new Square();
+            mySquare.theHeight(10);
+            mySquare.theWidth(10);
+            Console.WriteLine("My square has a value of: {0}", mySquare.area());
+            Console.ReadKey();
+
+            // Console.WriteLine("Hello World!");
         }
     }
 }
